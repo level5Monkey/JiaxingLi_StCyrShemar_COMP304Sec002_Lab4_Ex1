5fd50242608c03609f7e0a18ad087e8ccb07c114
Jiaxing Li <henrylovewatermelon@gmail.com>
2021-11-23 10:25:18 -0500

    Completed spinners in PatientActivity

--- app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/LoginActivity.java
+++ app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.jiaxingli_stcyrshemar_comp304sec002_lab4_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private NurseViewModel nurseViewModel;
     private String nurseID;
     private String userPassword;
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,13 @@ public class LoginActivity extends AppCompatActivity {
                 if (!nurseID.isEmpty() && !userPassword.isEmpty()) {
                     nurseViewModel.getVerifyResult(nurseID, userPassword).observe(LoginActivity.this, result -> {
                         if (result == 1) {
+
+                            SharedPreferences sharedPref = LoginActivity.this.getSharedPreferences(
+                                    MY_PREFS_NAME, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString("nurseID", nurseID);
+                            editor.apply();
+
                             Intent intentMain = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intentMain);
                         } else {
--- app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/PatientActivity.java
+++ app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/PatientActivity.java
@@ -1,19 +1,27 @@
 package com.example.jiaxingli_stcyrshemar_comp304sec002_lab4_ex1;
 
+import static com.example.jiaxingli_stcyrshemar_comp304sec002_lab4_ex1.LoginActivity.MY_PREFS_NAME;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class PatientActivity extends AppCompatActivity {
     private PatientViewModel patientViewModel;
+    private NurseViewModel nurseViewModel;
     private TextInputEditText firstName;
     private TextInputEditText lastName;
-    private TextInputEditText department;
+    private Spinner department;
     private TextInputEditText nurseID;
     private TextInputEditText room;
 
@@ -23,6 +31,13 @@ public class PatientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_patient);
 
         patientViewModel = new ViewModelProvider(this).get(PatientViewModel.class);
+        nurseViewModel = new ViewModelProvider(this).get(NurseViewModel.class);
+
+        SharedPreferences sharedPref = PatientActivity.this.getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE);
+        String nurseIDValue = sharedPref.getString("nurseID", "Nurse ID");
+
+        TextInputEditText nurseID=(TextInputEditText)findViewById(R.id.TextInputNurseID);
+        nurseID.setText(nurseIDValue);
 
         Button btnCreatePatient = (Button) findViewById(R.id.btnCreatePatient);
         btnCreatePatient.setOnClickListener(new View.OnClickListener() {
@@ -30,21 +45,53 @@ public class PatientActivity extends AppCompatActivity {
                 createPatientButton(v);
             }
         });
+
+        department = (Spinner)findViewById(R.id.SpinnerDepartment);
+        StringBuffer departmentName = new StringBuffer();
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.departments_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        department.setAdapter(adapter);
+        department.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                switch(pos)
+                {
+                    case 0:
+                        departmentName.append("Emergency");
+                        break;
+                    case 1:
+                        departmentName.append("Cardiology");
+                        break;
+                    case 2:
+                        departmentName.append("Nursing");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
     }
 
     public void createPatientButton(View view) {
         firstName = (TextInputEditText)findViewById(R.id.TextInputFirstName);
         lastName = (TextInputEditText)findViewById(R.id.TextInputLastName);
-        department = (TextInputEditText)findViewById(R.id.TextInputDepartment);
         nurseID = (TextInputEditText)findViewById(R.id.TextInputNurseID);
         room = (TextInputEditText)findViewById(R.id.TextInputRoom);
+
         if (firstName.getText().toString().length() != 0 && lastName.getText().toString().length() != 0
-                && department.getText().toString().length() != 0 &&
+                && department.toString().length() != 0 &&
                 nurseID.getText().toString().length() != 0 &&
                 room.getText().toString().length() != 0) {
             String firstNameValue = firstName.getText().toString();
             String lastNameValue = lastName.getText().toString();
-            String departmentValue = department.getText().toString();
+            String departmentValue = department.toString();
             int nurseIDValue = Integer.parseInt(nurseID.getText().toString());
             String roomValue = room.getText().toString();
 
