f6f61ffab352b8087921c1441be7e8117f7f9d23
shemar stcyr <shemar.stcyr2@gmail.com>
2021-11-23 23:42:17 -0500

    Completed Project

--- app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/ViewTestActivity.java
+++ app/src/main/java/com/example/jiaxingli_stcyrshemar_comp304sec002_lab4_ex1/ViewTestActivity.java
@@ -5,7 +5,10 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,6 +23,7 @@ public class ViewTestActivity extends AppCompatActivity {
     private TextView TEMP;
     private  TextView textviewinfo;
     private TestViewModel testview;
+    private Button home;
 
 
 
@@ -28,12 +32,8 @@ public class ViewTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_test);
 
-      //  testid = (TextView) findViewById(R.id.textViewTestID);
-      //  patientid = (TextView) findViewById(R.id.textViewPatientID);
-       // nurseid = (TextView) findViewById(R.id.textViewNurseID);
-       // BPL = (TextView) findViewById(R.id.textViewBPL);
-       // BPH = (TextView) findViewById(R.id.textViewBPH);
-       // TEMP = (TextView) findViewById(R.id.textViewTemp);
+
+        home = (Button) findViewById(R.id.buttonhome);
         textviewinfo = (TextView) findViewById(R.id.textViewTestInfo);
 
         testview = new ViewModelProvider(this).get(TestViewModel.class);
@@ -58,5 +58,9 @@ public class ViewTestActivity extends AppCompatActivity {
         //BPH.setText("BPH: "+test.getBPH());
         //TEMP.setText("Temperature: "+test.getTemperature());
 
+    }
+    public void HomeButton(View view ){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
